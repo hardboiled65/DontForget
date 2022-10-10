202df4c351204a0a63dcbfcec1b95d3f01fc36ed
Yujŏnja <hardboiled65@gmail.com>
2022-10-10 12:32:12 +0900

    selection-sort/c: Selection sort implementation.

--- array/c/array.h
+++ array/c/array.h
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 typedef struct df_array_t {
     int32_t *values;
@@ -25,6 +26,20 @@ df_array_t df_array_new(uint32_t length)
 void df_array_set(df_array_t *array, uint32_t index, int32_t value)
 {
     array->values[index] = value;
+}
+
+int32_t df_array_at(df_array_t *array, uint32_t index)
+{
+    assert(index < array->length);
+
+    return array->values[index];
+}
+
+void df_array_swap(df_array_t *array, uint32_t index1, uint32_t index2)
+{
+    int32_t tmp = array->values[index1];
+    array->values[index1] = array->values[index2];
+    array->values[index2] = tmp;
 }
 
 void df_array_print(df_array_t *array)
---
+++ selection-sort/c/main.c
@@ -0,0 +1,42 @@
+#include "../../array/c/array.h"
+
+void selection_sort(df_array_t *array)
+{
+    for (uint32_t i = 0; i < array->length; ++i) {
+        int32_t min_value = df_array_at(array, i);
+        uint32_t min_index = 0;
+
+        for (uint32_t j = i + 1; j < array->length; ++j) {
+            // Find minimal value.
+            if (min_value > df_array_at(array, j)) {
+                min_value = df_array_at(array, j);
+                min_index = j;
+            }
+        }
+
+        // Swap if less.
+        if (df_array_at(array, i) > min_value) {
+            df_array_swap(array, i, min_index);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    df_array_t array = df_array_new(5);
+
+    df_array_set(&array, 0, 64);
+    df_array_set(&array, 1, 25);
+    df_array_set(&array, 2, 12);
+    df_array_set(&array, 3, 22);
+    df_array_set(&array, 4, 11);
+
+    df_array_print(&array);
+
+    // Do sorting.
+    selection_sort(&array);
+
+    df_array_print(&array);
+
+    return 0;
+}
